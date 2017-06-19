ca0dbb8e448d85200e7d16389cb3e0342c04f4e4
JuanGabrieHincapieEscobar <jugahines12@gmail.com>
2017-06-19 16:48:04 -0500

    Add files via upload

---
+++ Api.openweathermap.py
@@ -0,0 +1,12 @@
+#JUAN GABRIEL
+import requests
+city = raw_input("ingrese el nombre de la ciudad:")
+url="http://api.openweathermap.org/data/2.5/weather?q=" +city+ "&appid=ec36addbebf58a73a09110a58df3a4d0"
+data = requests.get(url)
+read = data.json()
+print ("city name :    {}".format(read['name']))
+print ("temperature :  {}c".format(read['main']['temp']-273.15))
+print ("humidity:      {}".format(read['main']['humidity']))
+print ("pressure:      {}".format(read['main']['pressure']))
+print ("wind speed:    {}".format(read['wind']['speed']))
+print ("Description:   {}".format(read['weather'][0]['description']))
